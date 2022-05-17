121997ec4a921ff6ab07d74e55552fbfc7b4a7be
Daniel <dgarciademateosalcaide@gmail.com>
2022-05-17 21:30:11 +0200

    Clase dani

--- src/Password1.java
+++ src/Password1.java
@@ -1,4 +1,120 @@
-
+import java.util.*;
 public class Password1 {
 
+	private int longitud;
+	private String [] caracteres= {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+			   "K", "L", "M","N","O","P","Q","R","S","T","U","V","W", "X","Y","Z","a","b", "c", "d", "e", "f", "g", "h", "i", "j",
+			   "k", "l", "m","n","o","p","q","r","s","t","u","v","w", "x","y","z","0","1","2","3","4","5","6","7","8","9"  };
+	
+	
+	private String [] minaux={"a","b", "c", "d", "e", "f", "g", "h", "i", "j",
+		   "k", "l", "m","n","o","p","q","r","s","t","u","v","w", "x","y","z"};
+	
+	private String[] maaux= {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+			   "K", "L", "M","N","O","P","Q","R","S","T","U","V","W", "X","Y","Z"};
+	
+	private String [] numaux= {"0","1","2","3","4","5","6","7","8","9"};
+
+    private String contrasenia1;
+    
+	
+
+	
+	
+
+	public Password(int longitud) {
+		super();
+		this.longitud = longitud;
+		
+	}
+
+	public int getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	
+
+
+	public String getContrasenia1() {
+		return contrasenia1;
+	}
+	public void setContrasenia1(String contrasenia1) {
+		this.contrasenia1 = contrasenia1;
+	}
+
+
+
+
+	public String[] getCaracteres() {
+		return caracteres;
+	}
+
+	public void setCaracteres(String[] caracteres) {
+		this.caracteres = caracteres;
+	}
+
+
+
+
+	public void generarPassword() {
+		String contrasenia = "";
+		
+		for(int i=0; i<longitud; i++) {
+			int aleatorio=(int)(0+Math.random()*caracteres.length);
+		
+				
+				contrasenia=contrasenia+caracteres[aleatorio];
+			
+		}
+		contrasenia1=contrasenia;
+	}
+	
+	public boolean passwordValid(Password contrasenia5) {
+		boolean valida = false;
+		int mayuscula=0;
+		int minuscula=0;
+		int numero=0;
+		
+		
+
+			for(int i=0; i<contrasenia5.getLongitud(); i++) {
+					
+				for(int j=0; j<minaux.length; j++) {
+					if(contrasenia5.getCaracteres()[i].equals(minaux[j])) {
+						minuscula++;
+					}
+				}
+				for(int l=0; l<maaux.length; l++) {
+					if(contrasenia5.getCaracteres()[i].equals(maaux[l])) {
+						mayuscula++;
+					}
+					
+					for(int m=0; m<numaux.length; m++) {
+						if(contrasenia5.getCaracteres()[i].equals(numaux[m])) {
+							numero++;
+						}
+					}
+				}
+			
+		}//Fin bucle anidado
+		
+		if(mayuscula>=1 && minuscula>2 && numero>=1 ) {
+			valida=true;
+		}
+		
+		return valida;
+	
+	}
+
+	@Override
+	public String toString() {
+		return "Password [contrasenia1=" + contrasenia1 + "]";
+	}
+	
+	
+	
 }
