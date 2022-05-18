ce4ce1cd694920810b1b86217f38c0f63cca5f82
Emeterio Felguera <emelyoko934@gmail.com>
2022-05-18 13:24:03 +0200

    Terminar principalApp

--- src/Principal_App.java
+++ src/Principal_App.java
@@ -1,17 +1,18 @@
-//comentario Irene
+import java.util.*;
 public class Principal_App {
 	
+	static Scanner sc = new Scanner(System.in);
+	
 	public static void main(String[] args) {
+		int longitud=0;
 		
-		Password1 pasword1 = new Password1(8);
+		System.out.println("Introduce la longitud que quieres que tenga la contraseña: ");
+		longitud = sc.nextInt(); 
+		Password1 pasword1 = new Password1(longitud);
 		
 		pasword1.generarPassword();
 		System.out.println(pasword1);
-		
-
-		
-
-		
+	
 	if(pasword1.passwordValid(pasword1)==true) {
 		System.out.println("Contraseña valdia");
 	}else {
@@ -19,8 +20,4 @@ public class Principal_App {
 	}
 	
 	}
-
-	//lll
-
-	
 }
