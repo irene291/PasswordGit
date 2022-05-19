7149ec8f475e99295826b2aa4bd8acfc41c3fa1a
Irene <irenvilpe@gmail.com>
2022-05-19 21:16:27 +0200

    Codigo Password2 y PrincipalApp

--- src/Password2.java
+++ src/Password2.java
@@ -1,4 +1,110 @@
 
 public class Password2 {
 
+	private int longitud;
+	private String [] caracteres= {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+			   "K", "L", "M","N","O","P","Q","R","S","T","U","V","W", "X","Y","Z","a","b", "c", "d", "e", "f", "g", "h", "i", "j",
+			   "k", "l", "m","n","o","p","q","r","s","t","u","v","w", "x","y","z",".","_","*","ñ","Ñ"};
+	
+	private String [] minaux={"a","b", "c", "d", "e", "f", "g", "h", "i", "j",
+		   "k", "l", "m","n","o","p","q","r","s","t","u","v","w", "x","y","z"};
+	
+	private String[] maaux= {"A", "B", "C", "D", "E", "F", "G", "H", "I", "J",
+			   "K", "L", "M","N","O","P","Q","R","S","T","U","V","W", "X","Y","Z"};
+	
+	private String [] caresp= {".","_","*","ñ","Ñ"};
+	
+    private String contraseniaFinal;
+	
+
+	public Password2(int longitud) {
+		this.longitud = longitud;
+		
+	}
+
+	public int getLongitud() {
+		return longitud;
+	}
+
+	public void setLongitud(int longitud) {
+		this.longitud = longitud;
+	}
+
+	public String getContraseniaFinal() {
+		return contraseniaFinal;
+	}
+	
+	public void setContrasenia1(String contrasenia1) {
+		this.contraseniaFinal = contrasenia1;
+	}
+	
+	
+
+	public void generarPassword() {
+		String contrasenia = "";
+		
+		for(int i=0; i<longitud; i++) {
+			int aleatorio=(int)(0+Math.random()*caracteres.length);
+			
+				contrasenia=contrasenia+caracteres[aleatorio];
+			
+		}
+		
+		contraseniaFinal=contrasenia;
+		
+	}
+
+	
+	
+
+	public boolean passwordValid(Password2 contraseniaF) {
+		boolean valida = false;
+		int mayuscula=0;
+		int minuscula=0;
+		int caracter=0;
+		
+		
+
+			for(int i=0; i<contraseniaF.getLongitud(); i++) {
+					
+				String cara=String.valueOf(contraseniaF.getContraseniaFinal().charAt(i));
+				
+				for(int j=0; j<minaux.length; j++) {
+					
+					
+					if(cara.equals(minaux[j])) {
+						minuscula++;
+					
+					}
+				}
+				for(int l=0; l<maaux.length; l++) {
+					if(cara.equals(caracteres[l])) {
+						mayuscula++;
+					}
+					
+				}
+				
+				for(int m=0; m<caresp.length; m++) {
+					if(cara.equals(caresp[m])) {
+						caracter++;
+					}
+				}
+			
+		}//Fin bucle anidado
+		
+		if(mayuscula>=1 && minuscula>2 && caracter>=1 ) {
+			valida=true;
+		}
+		
+		return valida;
+	
+	}
+
+	@Override
+	public String toString() {
+		return "Password [contrasenia=" + contraseniaFinal + "]";
+	}
+	//prueba
+	
+	
 }
--- src/Principal_App.java
+++ src/Principal_App.java
@@ -6,18 +6,31 @@ public class Principal_App {
 	public static void main(String[] args) {
 		int longitud=0;
 		
-		System.out.println("Introduce la longitud que quieres que tenga la contraseña: ");
+		System.out.println("Introduce la longitud que quieres que tenga las contraseñas: ");
 		longitud = sc.nextInt(); 
+		
 		Password1 pasword1 = new Password1(longitud);
 		
-		pasword1.generarPassword();
-		System.out.println(pasword1);
-	
-	if(pasword1.passwordValid(pasword1)==true) {
-		System.out.println("Contraseña valdia");
-	}else {
-		System.out.println("NO VALIDA");
-	}
-	
+			pasword1.generarPassword();
+			System.out.println("Primera contraseña: " +  pasword1);
+			
+			if(pasword1.passwordValid(pasword1)==true) {
+				System.out.println("Valdia, la contraseña es fuerte");
+			}else {
+				System.out.println("NO VALIDA. La contraseña no es fuerte");
+			}
+			
+
+			Password2 pasword2 = new Password2(longitud);
+			
+			pasword2.generarPassword();
+			System.out.println("Segunda contraseña: " +  pasword2);
+			
+			if(pasword2.passwordValid(pasword2)==true) {
+				System.out.println("Valdia, la contraseña es fuerte");
+			}else {
+				System.out.println("NO VALIDA. La contraseña no es fuerte");
+			}
+		
 	}
 }
